c893f14e7e3e7e1ffc8f7809da6c88817c0a0875
Joha Kim <johakim9936@gmail.com>
2020-07-06 18:49:00 -0400

    practice Angular http requests using Firebase

---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators";
+import { Post } from "./post.model";
+import { PostsService } from "./posts.service";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent implements OnInit {
+  loadedPosts: Post[] = [];
+  isFetchingPost = false;
+
+  constructor(private http: HttpClient, private postsService: PostsService) {}
+
+  ngOnInit() {
+    this.isFetchingPost = true;
+    this.postsService.fetchPosts().subscribe((posts) => {
+      this.isFetchingPost = false;
+      this.loadedPosts = posts;
+    });
+  }
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+    // requests are only sent when you subscribe
+    this.postsService.createAndStorePost(postData.title, postData.content);
+  }
+
+  onFetchPosts() {
+    // Send Http request
+    // get requests are only processed when you subscribe
+    this.isFetchingPost = true;
+    this.postsService.fetchPosts().subscribe((posts) => {
+      this.isFetchingPost = false;
+      this.loadedPosts = posts;
+    });
+  }
+
+  onClearPosts() {
+    // Send Http request
+    this.postsService.deletePosts().subscribe(() => {
+      this.loadedPosts = []; 
+    }); 
+  }
+}
